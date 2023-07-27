8e69b0538368491d16c2dbf3a4cbb7cf2b19b8f9
Jonatan Lifshits <djonli2003@gmail.com>
2023-07-27 18:39:14 +0300

    Initial commit: async/await

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,14 @@
+﻿await PrintAsync();
+Console.WriteLine("Some actions in main...\n");
+
+void Print() {
+    Thread.Sleep(3000);
+    Console.WriteLine("Hello World!");
+}
+
+async Task PrintAsync()
+{
+    Console.WriteLine("Start method PrintAsync");
+    await Task.Run(() => { Print(); });
+    Console.Write("End method PrintAsync\n");
+}
